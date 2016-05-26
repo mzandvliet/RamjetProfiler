c55e059cec14a015fc85320e11a04f8de0c50eb5
mzandvliet <martijn@ramjetanvil.com>
2016-05-26 16:52:56 +0200

    First test

---
+++ Assets/Plugins/Profiler/RamjetProfiler.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+using Debug = UnityEngine.Debug;
+
+public static class RamjetProfiler {
+    private static Stopwatch _stopwatch;
+    private static string _name;
+
+    static RamjetProfiler() {
+        _stopwatch = new Stopwatch();
+    }
+
+    public static void BeginSample(string name) {
+        _name = name;
+        _stopwatch.Reset();
+        _stopwatch.Start();
+    }
+
+    public static void EndSample() {
+        _stopwatch.Stop();
+        Debug.LogFormat("Profiler || {0} took {1:0.00}", _name, _stopwatch.Elapsed.TotalSeconds * 1000d);
+        _name = "";
+    }
+}
---
+++ Assets/Scripts/MyComponent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class MyComponent : MonoBehaviour {
+    private void Update() {
+        int j = 0;
+        for (int i = 0; i < 1000000; i++) {
+            j++;
+        }
+        MyNestedFunction();
+    }
+
+    private void MyNestedFunction() {
+        int j = 0;
+        for (int i = 0; i < 2000000; i++) {
+            j++;
+        }
+    }
+}
